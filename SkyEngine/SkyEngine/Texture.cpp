0ca96583fc3e5f759529a73256bf7b731077efc9
mccannd <mccannd@seas.upenn.edu>
2017-12-04 18:11:35 -0500

    jitter, to be replaced with flow noise

--- SkyEngine/SkyEngine/Texture.cpp
+++ SkyEngine/SkyEngine/Texture.cpp
@@ -320,7 +320,7 @@ void Texture3D::createSampler() {
     samplerInfo.addressModeU = VK_SAMPLER_ADDRESS_MODE_REPEAT;
     samplerInfo.addressModeV = VK_SAMPLER_ADDRESS_MODE_REPEAT;
     samplerInfo.addressModeW = VK_SAMPLER_ADDRESS_MODE_REPEAT;
-    samplerInfo.anisotropyEnable = VK_TRUE;
+    samplerInfo.anisotropyEnable = VK_FALSE;
     samplerInfo.maxAnisotropy = 16;
     samplerInfo.borderColor = VK_BORDER_COLOR_INT_OPAQUE_BLACK;
     samplerInfo.unnormalizedCoordinates = VK_FALSE;
