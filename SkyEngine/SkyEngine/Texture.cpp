6d2f9ba7e945c06bef58aa5e756ce6723dd1f5ef
mccannd <mccannd@seas.upenn.edu>
2017-12-06 03:02:57 -0500

    started reorganizing comp, moved to model more closely matching paper, nearly eliminated banding artifacts

--- SkyEngine/SkyEngine/Texture.cpp
+++ SkyEngine/SkyEngine/Texture.cpp
@@ -321,11 +321,11 @@ void Texture3D::createSampler() {
     samplerInfo.addressModeV = VK_SAMPLER_ADDRESS_MODE_REPEAT;
     samplerInfo.addressModeW = VK_SAMPLER_ADDRESS_MODE_REPEAT;
     samplerInfo.anisotropyEnable = VK_FALSE;
-    samplerInfo.maxAnisotropy = 16;
+    samplerInfo.maxAnisotropy = 1.0;
     samplerInfo.borderColor = VK_BORDER_COLOR_INT_OPAQUE_BLACK;
     samplerInfo.unnormalizedCoordinates = VK_FALSE;
-    samplerInfo.compareEnable = VK_FALSE;
-    samplerInfo.compareOp = VK_COMPARE_OP_ALWAYS;
+    //samplerInfo.compareEnable = VK_FALSE;
+    samplerInfo.compareOp = VK_COMPARE_OP_NEVER;
 
     samplerInfo.mipmapMode = VK_SAMPLER_MIPMAP_MODE_LINEAR;
     samplerInfo.mipLodBias = 0.0f;
@@ -522,7 +522,7 @@ void Texture3D::initFromFile(std::string path) {
         stbi_image_free(pixels);
     }
 
-    createImage(width, height, depth, VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_SAMPLED_BIT, imageFormat, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT);
+    createImage(width, height, depth, VK_IMAGE_USAGE_SAMPLED_BIT, imageFormat, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT);
 
     transitionImageLayout(textureImage, imageFormat, VK_IMAGE_LAYOUT_UNDEFINED, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL);
     copyBufferToImage(stagingBuffer, textureImage, static_cast<uint32_t>(width), static_cast<uint32_t>(height), static_cast<uint32_t>(depth));
